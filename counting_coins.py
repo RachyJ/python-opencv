939a50085569df620360a20ee4edbfb8b1761d74
rachyj <beirikui@qq.com>
2020-03-08 15:19:31 +0800

    print coins

--- counting_coins.py
+++ counting_coins.py
@@ -17,3 +17,17 @@ coins = image.copy()
 cv2.drawContours(coins, cnts, -1, (0, 255, 0), 2)
 cv2.imshow("Coins", coins)
 cv2.waitKey(0)
+
+for (i, c) in enumerate(cnts):
+    (x, y, w, h) = cv2.boundingRect(c)
+
+    print("Coin #%d" % (i+1))
+    coin = image[y:y + h, x:x + w]
+
+    mask = np.zeros(image.shape[:2], dtype = "uint8")
+    ((centerX, centerY), radius) = cv2.minEnclosingCircle(c)
+    cv2.circle(mask, (int(centerX), int(centerY)), int(radius), 255, -1)
+    mask = mask[y: y+h, x:x+w]
+
+    cv2.imshow("Mask coin", cv2.bitwise_and(coin, coin, mask = mask))
+    cv2.waitKey(0)
